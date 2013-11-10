68d7b848213ea4d05d800ac0a8158d37a8b91ef1
Stewart Taylor <stewart.taylor1@gmail.com>
2013-11-10 06:21:20 -0800

    Updated matchmaker UI

--- Android/SketchBattle/src/io/platypus/MatchMakerActivity.java
+++ Android/SketchBattle/src/io/platypus/MatchMakerActivity.java
@@ -15,6 +15,8 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MatchMakerActivity extends Activity {
@@ -24,6 +26,7 @@ public class MatchMakerActivity extends Activity {
 	public Player currentPlayer;
 
 	private int dotCount = 1;
+	private float rot = 0f;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -33,13 +36,18 @@ public class MatchMakerActivity extends Activity {
 		this.currentPlayer = (Player) intent.getParcelableExtra("player");
 
 		setContentView(R.layout.waiting_view);
+		
+		
+		View button = findViewById(R.id.btn_menu);
+		button.setVisibility(View.GONE);
 
 		try {
 			matchMaker();
 		} catch (IOException e) {
 			Log.e("MatchMaker", e.getMessage());
 		}
-		waitingTextAnimation();
+	//	waitingTextAnimation();
+		waitingImageAnimation();
 		// moveToColorScreen();
 	}
 
@@ -90,6 +98,8 @@ public class MatchMakerActivity extends Activity {
 		game_endpoint.addValueEventListener(participantListener);
 			
 	}
+	
+	
 
 	private void waitingTextAnimation() {
 		final Handler handler = new Handler();
@@ -99,6 +109,7 @@ public class MatchMakerActivity extends Activity {
 				// Do something after 5s = 5000ms
 				// buttons[inew][jnew].setBackgroundColor(Color.BLACK);
 				TextView findingText = (TextView) findViewById(R.id.drawobject_text);
+				ImageView imageView = (ImageView) findViewById(R.id.img_waiting);
 
 				String ftext = "Finding game";
 
@@ -116,6 +127,21 @@ public class MatchMakerActivity extends Activity {
 				waitingTextAnimation();
 			}
 		}, 800);
+	}
+	
+	
+	private void waitingImageAnimation() {
+		final Handler handler = new Handler();
+		handler.postDelayed(new Runnable() {
+			@Override
+			public void run() {
+				ImageView imageView = (ImageView) findViewById(R.id.img_waiting);
+				imageView.setRotation(rot);
+				rot += 2f;
+				
+				waitingImageAnimation();
+			}
+		}, 20);
 	}
 
 	private void moveToColorScreen(Game game) {
